6daed41ade883321b94bf0b6ad83157ca6034dba
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-07-20 17:08:30 +0300

    Add user views from devise
    The views handle the user authentication and authorization functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'events/index'
+
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+  }
+
+
+    get 'events/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

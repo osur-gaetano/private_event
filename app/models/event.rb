c069667bc4c11ff11608ba283874f8ad47946329
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-07-30 22:26:55 +0300

    Add validations to Events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
+  validates :location, presence: true, length: {minimum:6, maximum:20}
+  validates :about, presence: true
+  validates :title, presence: true
+  validates :event_date, presence: true
+
+
   belongs_to :creator, class_name: "User"
 
   has_many :attendee_events, foreign_key: :attended_event_id
